080f3466cbc440e83e15af22440189a82c152f08
VytorCalixto <vytorcalixto@gmail.com>
2015-03-07 21:39:27 -0300

    Nova versão

--- www/js/app.js
+++ www/js/app.js
@@ -1,56 +1,29 @@
-var vsbc = angular.module('vsbc14', ['ui.router']);
-
-vsbc.config(function($stateProvider, $urlRouterProvider){
-    $stateProvider
-    .state('home',{
-        url: '/home',
-        templateUrl: 'templates/home.html'
-    })
-    .state('trabalhos',{
-        url: '/trabalhos',
-        templateUrl: 'templates/trabalhos.html',
-        controller: 'SiteCtrl'
-    })
-    .state('projetos',{
-        url: '/projetos',
-        templateUrl: 'templates/projetos.html',
-        controller: 'SiteCtrl'
-    })
-    .state('contato',{
-        url: '/contato',
-        templateUrl: 'templates/contato.html'
-    });
-
-    $urlRouterProvider.otherwise('/home');
-});
+var vsbc = angular.module('vsbc14', []);
 
 vsbc.controller('SiteCtrl',function($scope, $http){
     $scope.projetos = [];
-    $scope.trabalhos = [];
-    $scope.ghSuccess = false;
-    $scope.trabSuccess = false;
-    $scope.loadingGh = true;
-    $scope.loadingTrab = true;
 
     $http.get('https://api.github.com/users/vytorcalixto/repos')
     .success(function (data, status, headers, config) {
         $scope.projetos = data;
-        if ($scope.projetos.length > 0) {
-            $scope.ghSuccess = true;
-        }
-    })
-    .then(function () {
-        $scope.loadingGh = false;
     });
 
     $http.get('js/trabalhos.json')
     .success(function (data, status, headers, config) {
         $scope.trabalhos = data;
-        if($scope.trabalhos.length > 0){
-            $scope.trabSuccess= true;
-        }
-    })
-    .then(function () {
-        $scope.loadingTrab = false;
     });
+
+    $scope.getAge = function(){
+    	var birthDate = new Date('1996-06-16T00:00:00.000Z');
+        var otherDate = new Date();
+
+        var years = (otherDate.getFullYear() - birthDate.getFullYear());
+
+        if (otherDate.getMonth() < birthDate.getMonth() ||
+                otherDate.getMonth() === birthDate.getMonth() && otherDate.getDate() < birthDate.getDate()) {
+            years--;
+        }
+
+        return years;
+    }
 });
